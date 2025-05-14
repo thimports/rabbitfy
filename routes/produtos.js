91a961d76f0f747a0efaa60aad3e31be1e2b9d6d
thimports <thimports1203@gmail.com>
2025-05-14 16:46:46 -0300

    Delete routes directory

--- routes/produtos.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json([{ nome: 'Produto Exemplo', preco: 99.90 }]);
-});
-
-module.exports = router;
